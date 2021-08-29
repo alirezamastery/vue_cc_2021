806852d4c6efe98acdd401d7f614029c1f898dcc
alireza <alireza.mastery@gmail.com>
2021-08-29 10:40:53 +0430

    initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header>
+    <h1>Task tracker</h1>
+    <h1>{{title}}</h1>
+    <Button @btn-click="$emit('toggle-add-task')"
+            :text="showAddTask ? 'close' : 'Add Task'"
+            :color="showAddTask ? 'red' : 'green'" />
+<!--    <Button text="update task" color="blue"/>-->
+<!--    <Button text="delete task" color="red"/>-->
+  </header>
+</template>
+
+<script>
+import Button from "@/components/Button";
+export default {
+  name:'Header',
+  props: {
+    title: String,
+    showAddTask:Boolean
+  },
+  components:{
+    Button,
+  }
+}
+</script>
